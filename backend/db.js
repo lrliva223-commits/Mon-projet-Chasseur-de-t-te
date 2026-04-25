8ca92338ee19e3232e12e4f717a5f9c1b66d078b
lrliva223-commits <lrliva223@gmail.com>
2026-04-25 16:24:29 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -10,7 +10,10 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 db.connect((err) => {
   if (err) {
