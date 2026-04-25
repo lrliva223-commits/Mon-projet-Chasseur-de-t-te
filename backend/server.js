0cfa4d7c65e2c91d6df1c6e0ff2f19493f76b1cf
lrliva223-commits <lrliva223@gmail.com>
2026-04-25 15:04:58 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 const { upload, uploadErrorHandler } = require('./middleware/upload');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://chasseur-de-tete.onrender.com' // L'URL de votre frontend sur Vercel
+}));
 app.use(express.json());
 
 // Servir les fichiers statiques du frontend
